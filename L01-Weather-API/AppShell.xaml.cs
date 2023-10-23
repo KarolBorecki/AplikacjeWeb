7079ec5327d1ddba68bd2e7767046c331ce15bef
Karol Borecki <karol.borecki@onet.eu>
2023-10-23 09:19:45 +0200

    Added video, deleted unusedd projects

--- L01-Weather-API/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace L01_Weather_API;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- L01-Weather-API/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace L01_Weather_API;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
